e4b89112271bd0b6dda5305e3ff877d5cad8a755
Kang-Seungtaek <stkang0108@gmail.com>
2020-12-02 00:40:45 +0900

    [feat/#2]:prisma/signup_resolver&schema setting

---
+++ graphql/User/signup/signup.js
@@ -0,0 +1,17 @@
+import { prisma } from "../../../src/generated/prisma-client";
+import * as bcrypt from "bcryptjs";
+
+export default {
+  Mutation: {
+    signup: async (_, args) => {
+      const { email, password } = args;
+      const exists = await prisma.$exists.user({ OR: [{ email }] });
+      if (exists) {
+        throw Error("Already exists Email");
+      }
+      const hashedPassword = await bcrypt.hash(password, 5);
+      await prisma.createUser({ email, password: hashedPassword });
+      return true;
+    },
+  },
+};
